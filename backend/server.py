b90cc2ee96161dc86a243b108507049bfe25ae13
yilkid <yillkid@gmail.com>
2020-12-09 22:22:28 +0800

    change port number

--- backend/server.py
+++ backend/server.py
@@ -335,4 +335,4 @@ def dashboard():
     return render_template('dashboard.html',title=content)
 
 if __name__ == "__main__":
-    app.run(debug = True, threaded = True, host = "0.0.0.0", port = 5000)
+    app.run(debug = True, threaded = True, host = "0.0.0.0", port = 5001)
